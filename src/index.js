b9e0d3e981d1a3074fc832ea9f549f613cb28be6
ryaningham2001 <ryaningham2001@gmail.com>
2019-03-13 19:57:53 +0700

    ban command

--- src/index.js
+++ src/index.js
@@ -85,11 +85,13 @@ client.on('message', async msg => {
                 .addField("Reason", `${kickReason}`);
             
             let kickChannel = msg.guild.channels.find(`name`, "logs");
-            if (!kickChannel) return msg.channel.send("Can't find incidents channel.");
+            if (!kickChannel) return msg.channel.send("Can't find logs channel.");
 
             msg.guild.member(user).kick(kickReason);
             kickChannel.send(kickEmbed);
             msg.channel.send(kickEmbed);
+
+            console.log(`${msg.guild.name}: user ${user.name} was kicked.`);
             
         }
 
@@ -100,7 +102,24 @@ client.on('message', async msg => {
             let banReason = args.join(" ").slice(22);
             if(!msg.member.hasPermission("ADMINISTRATOR")) return msg.channel.send("No permissions, retard");
             if(user.hasPermission("ADMINISTRATOR")) return msg.channel.send("That person can't be kicked, retard");
+
+            let banEmbed = new Discord.RichEmbed()
+                .setDescription("~Ban~")
+                .setColor('#e56b00')
+                .addField("Banned User", `${user} with ID ${user.id}`)
+                .addField("Banned by" , `<@${msg.author.id}> with ID ${message.author.id}`)
+                .addField("Banned in", `${msg.channel}`)
+                .addField("Time", `${msg.createdAt}`)
+                .addField("Reason", `${banReason}`);
             
+                let logs = msg.guild.channels.find('name', "logs");
+                if (!banChannel) return msg.channel.send("Can't find logs channel");
+
+                msg.guild.member(user).ban(banReason);
+                logs.send(banEmbed);
+                msg.channel.send(banEmbed);
+
+                console.log(`${msg.guild.name}: user ${user.name} was banned.`);
         }
     }
     
