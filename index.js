3094dcbedbdb0472508842d74929bde4d6bf2558
ryaningham2001 <ryaningham2001@gmail.com>
2019-03-15 23:01:12 +0700

    got command handler working im dum

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ fs.readdir("./commands", (err, files) => {
         let props = require(`./commands/${f}`);
         console.log(`${f} loaded`);
         bot.commands.set(props.help.name, props);
-        console.log(props);
     });
 });
 
